f28606717423dcae46a7cbd3cce200e5f6944f78
FedeCasper <federicorouyere@gmail.com>
2023-06-06 12:35:55 -0300

    Creating filter

--- main.js
+++ main.js
@@ -3,12 +3,22 @@ const { createApp } = Vue
 createApp({
      data() {
           return {
-               message: 'Hello Vue!',
-               movies: []
+               inputValue: "",
+               movies: [],
+               filteredMovies: []
           }
      },
      created(){
           this.movies = data.movies
           console.log(this.movies);
+     },
+     computed:{
+          filter(){
+               this.movies.forEach(movie => movie.optional_title == undefined ? movie.optional_title = "no-title" : movie.optional_title)
+               this.filteredMovies = this.movies.filter(movie => movie.title.toString().toLowerCase().includes(this.inputValue.toLowerCase()) || 
+               movie.optional_title.toString().toLowerCase().includes(this.inputValue.toLowerCase() )
+               )
+               this.filteredMovies.length == 0 ? this.filteredMovies = this.movies : ""
+          }
      }
 }).mount('#app')
