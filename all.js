152afb88d47793d72dc279772a81035674e42ecb
Federico Rouyere <101667265+FedeCasper@users.noreply.github.com>
2023-06-11 15:35:55 -0300

    Update all.js

--- all.js
+++ all.js
@@ -18,16 +18,16 @@ createApp({
           filter(){
                
                this.movies.forEach(movie => movie.optional_title == undefined ? movie.optional_title = "no-title" : movie.optional_title)
-               if(this.inputValue != "" && this.inputValue.length == 1){
-                    this.filteredMovies = this.movies.filter(movie => movie.title.toString().toLowerCase().startsWith(this.inputValue.toLowerCase()) || 
+               
+                    
 
-               movie.optional_title.toString().toLowerCase().startsWith(this.inputValue.toLowerCase()
-               }else{
+               
+               
                this.filteredMovies = this.movies.filter(movie => movie.title.toString().toLowerCase().includes(this.inputValue.toLowerCase()) || 
                movie.optional_title.toString().toLowerCase().includes(this.inputValue.toLowerCase() )
                )
                this.filteredMovies.length == 0 ? this.filteredMovies = this.movies : ""
-                    }
+                    
           }
      }
 }).mount('#app')
