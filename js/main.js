58172fb1dc0ca64b7be326a04864cb29fbfe6b6a
FedeCasper <federicorouyere@gmail.com>
2024-02-03 12:05:51 -0300

    Function jason converter

--- js/main.js
+++ js/main.js
@@ -72,6 +72,52 @@ createApp({
                     books: arrayOfClasification[3].length,
                     cortos: arrayOfClasification[1].length
                }
+          },
+     handleFileSelect(event) {
+          const file = event.target.files[0];
+          if (file) {
+               this.convertToJson(file)
+                    .then(objects => {
+                         console.log(objects); // Array de objetos
+                         // Aquí puedes hacer lo que necesites con el array de objetos
+                    })
+                    .catch(error => {
+                         console.error(error);
+                    });
+               }
+          },
+          
+          convertToJson(file) {
+          return new Promise((resolve, reject) => {
+               const reader = new FileReader();
+
+               reader.onload = (e) => {
+                    const data = new Uint8Array(e.target.result);
+                    const workbook = XLSX.read(data, { type: 'array' });
+
+                    const sheetName = workbook.SheetNames[0];
+                    const worksheet = workbook.Sheets[sheetName];
+
+                    const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+                    // Convertir el array de arrays en array de objetos
+                    const keys = json[0]; // Suponiendo que la primera fila contiene los nombres de las columnas
+                    const objects = json.slice(1).map(row => {
+                         return keys.reduce((obj, key, index) => {
+                              obj[key] = row[index];
+                              return obj;
+                         }, {});
+                    });
+
+                    resolve(objects);
+               };
+
+               reader.onerror = (error) => {
+                    reject(error);
+               };
+
+               reader.readAsArrayBuffer(file);
+          });
           }
      },
 
